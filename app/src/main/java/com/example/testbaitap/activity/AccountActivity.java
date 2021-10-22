0c720d04ce6259699962004711b5b3aee14c01e2
nhatm <nhatquan1702>
2021-10-22 19:35:29 +0700

    Thêm giao diện account

--- app/src/main/java/com/example/testbaitap/activity/AccountActivity.java
+++ app/src/main/java/com/example/testbaitap/activity/AccountActivity.java
@@ -1,12 +1,14 @@
 package com.example.testbaitap.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.testbaitap.R;
@@ -22,11 +24,10 @@ public class AccountActivity extends AppCompatActivity {
         editor = sharedPreferences.edit();
         Toast.makeText(AccountActivity.this, sharedPreferences.getString("email", "username"), Toast.LENGTH_SHORT).show();
 
-        Button button = findViewById(R.id.button);
+        CardView button = findViewById(R.id.cvDKTK);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
                 editor.remove("email");
                 editor.remove("password");
